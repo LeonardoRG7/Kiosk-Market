8c791244c46dbeaabe633584dc4ecc16091991c5
LeonardoRG7 <leonardoriascos14@gmail.com>
2024-04-03 23:28:18 -0500

    Login interface

--- kiosk-market/src/app/modules/auth/login/login.component.ts
+++ kiosk-market/src/app/modules/auth/login/login.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _toastrService: ToastrService) {}
 
   ngOnInit(): void {
+    this._toastrService.success('Prrueba', 'prueba');
   }
-
 }
